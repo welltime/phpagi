d44e9e508ec63aa6ea1767ccc463811898b1c233
Sebastian Berm <sebastian@sebsoft.nl>
2017-07-13 12:13:49 +0200

    PHP 7 compatibility
    Update to make sure this is more compatible with newer apps

--- phpagi-asmanager.php
+++ phpagi-asmanager.php
@@ -97,6 +97,11 @@
      * @var boolean
      */
     private $_logged_in = FALSE;
+   
+    public function setPagi(&$agi)
+    {
+      $this->pagi = $agi;
+    }
 
    /**
     * Constructor
@@ -104,7 +109,7 @@
     * @param string $config is the name of the config file to parse or a parent agi from which to read the config
     * @param array $optconfig is an array of configuration vars and vals, stuffed into $this->config['asmanager']
     */
-    function AGI_AsteriskManager($config=NULL, $optconfig=array())
+    function __construct($config=NULL, $optconfig=array())
     {
       // load config
       if(!is_null($config) && file_exists($config))
