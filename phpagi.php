2993fb3ea64c0d86fe0a36c911db671d46e15050
Sebastian Berm <sebastian@sebsoft.nl>
2017-07-13 12:16:59 +0200

    PHP 7 compatibility
    Trying to access private pagi variable is not possible. So added a setter for this.

--- phpagi.php
+++ phpagi.php
@@ -1550,7 +1550,7 @@ class AGI
     function &new_AsteriskManager()
     {
         $this->asm = new AGI_AsteriskManager(NULL, $this->config);
-        $this->asm->pagi =& $this;
+        $this->asm->setPagi($this);
         $this->config =& $this->asm->config;
         return $this->asm;
     }
