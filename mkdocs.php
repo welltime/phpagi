08ed43331eb8b61a35b2b9efccf6e13ca63d4115
Dmitry V. Alekseev <mail@alexeevdv.ru>
2018-12-07 14:55:02 +0300

    add composer.json

--- mkdocs.php
+++ mkdocs.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
   $cwd = getcwd();
-  passthru("phpdoc -t \"$cwd/api-docs\" -f \"$cwd/phpagi.php,$cwd/phpagi-asmanager.php,$cwd/phpagi-fastagi.php\" -pp on -ti \"phpAGI\" -o HTML:frames:earthli -s on");
+  passthru("phpdoc -t \"$cwd/api-docs\" -f \"$cwd/src/phpagi.php,$cwd/src/phpagi-asmanager.php,$cwd/src/phpagi-fastagi.php\" -pp on -ti \"phpAGI\" -o HTML:frames:earthli -s on");
 ?>
 
 
